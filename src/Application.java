d8bb23a06243be2e6eca04ffbc992dd73d8392b6
sudianer <32158128+sudianer@users.noreply.github.com>
2017-09-21 14:09:32 +0400

    Added entry point

---
+++ src/Application.java
@@ -0,0 +1,8 @@
+/**
+ * Created by U_LIVT35412 on 21.09.2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I'm Долгова Татьяна and i’m not a monkey ");
+    }
+}
